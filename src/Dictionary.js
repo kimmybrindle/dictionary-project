594cc9d36f42660822c82e16e14f60010ee3bb1d
kimmybrindle <kimmybrindle@gmail.com>
2026-05-25 17:13:08 -0700

    added axios and search to actual keyword data

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,23 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response.data.meanings[0]);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword} definition`);
+
+    // documentation: https://www.shecodes.io/learn/apis/dictionary
+    let apiKey = "5c04157d3b5bt2eeaa66db333fod0d64";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
